ae94c12732926a5bdd51c266055bd69976e874f5
luiz <luizalthmandev@gmail.com>
2025-05-26 11:32:20 -0300

    Fix: Bugs in images. Fix in next.config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
-            new URL('https://kjayxhdxemst85ys.public.blob.vercel-storage.com'),
+            {
+                protocol: 'https',
+                hostname: 'kjayxhdxemst85ys.public.blob.vercel-storage.com',
+                pathname: '/**', // permite qualquer imagem dentro do domínio
+            },
         ],
     },
 };
