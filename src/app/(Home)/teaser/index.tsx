483bf28f4d01c99f8fbe95ca3ac6a4342409ee63
luiz <luizalthmandev@gmail.com>
2025-06-24 12:11:53 -0300

    Fix: Styles text from teaser section

--- src/app/(Home)/teaser/index.tsx
+++ src/app/(Home)/teaser/index.tsx
@@ -3,13 +3,13 @@ import TeaserModal from '@/components/teaserModal';
 export default function Teaser() {
     return (
         <section className="w-full my-10 md:my-20 md:px-32 px-6 flex items-center justify-center flex-col">
-            <p className="text-xl text-purple-900/80 md:mb-5 md:w-1/2 text-center">
-                Assista ao teaser exclusivo e sinta a energia contagiante do
-                samba que só o Parakundê entrega!
-            </p>
             <h2 className="text-xl md:text-3xl font-bold text-purple-900 text-center md:mb-10 mb-5 uppercase">
                 Conheça o Parakundê
             </h2>
+            <p className="text-xl text-purple-900/80 mb-5 md:w-1/2 text-center">
+                Assista ao teaser exclusivo e sinta a energia contagiante do
+                samba que só o Parakundê entrega!
+            </p>
 
             <TeaserModal />
         </section>
